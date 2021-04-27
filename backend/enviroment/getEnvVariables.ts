c08cb5d3076a7b3962b81ad0eafda0160ae9854e
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2021-04-27 21:44:09 +0300

    debug: print mongodb url on load

--- backend/enviroment/getEnvVariables.ts
+++ backend/enviroment/getEnvVariables.ts
@@ -8,16 +8,19 @@ const getEnvVariables = () => {
 
     if (processType === 'production' || processType === undefined) {
 
-        require('dotenv').config({ path: configPath + "/prod.env"})
+        require('dotenv').config({path: configPath + "/prod.env"})
 
     } else if (processType === 'development') {
 
-        require('dotenv').config({ path: configPath + "/dev.env"})
+        require('dotenv').config({path: configPath + "/dev.env"})
 
     } else if (processType === 'test') {
 
-        require('dotenv').config({ path: configPath + "/test.env"})
+        require('dotenv').config({path: configPath + "/test.env"})
     }
+
+    console.log("processType = ", processType);
+    console.log("MONGODB_URL",process.env.MONGODB_URL);
 }
 
 export default getEnvVariables;
